4aede6e92f0133ffc87676f02f3e986398260290
Yuren Ju <yurenju@gmail.com>
2020-08-07 23:41:45 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,100 @@
+const sequence = [
+  [0, false],
+  [0, true],
+  [1, false],
+  [1, true],
+  [2, false],
+  [3, false],
+  [3, true],
+  [4, false],
+  [4, true],
+  [5, false],
+  [5, true],
+  [6, false],
+];
+
+function onMessage(event) {
+  const [channel, number, value] = event.data;
+  const el = document.querySelector(`[data-number="${number}"]`);
+  console.log("channel", channel, "number", number, "value", value);
+
+  if (channel < 144) {
+    // note off
+    el.classList.remove("active");
+  } else if (channel < 160) {
+    // note on
+    el.classList.add("active");
+  } else if (channel < 208 && channel > 191) {
+    // control change
+    if (number === 64) {
+      const pressedKeys = [...document.getElementsByClassName("active")];
+      if (value >= 64) {
+        pressedKeys.forEach((key) => key.classList.add("pedaled"));
+      } else {
+        pressedKeys.forEach((key) => key.classList.remove("pedaled"));
+      }
+    }
+  }
+}
+
+function renderPiano(el) {
+  let whiteKeys = [];
+  let blackKeys = [];
+  for (let i = 21; i < 109; i++) {
+    const index = i % 12;
+    const pos = sequence[index];
+    if (pos[1]) {
+      blackKeys.push(`<div data-number="${i}"></div>`);
+    } else {
+      whiteKeys.push(`<div data-number="${i}"></div>`);
+    }
+  }
+  el.innerHTML +=
+    `<div class="black-keys">${blackKeys.join("")}</div>` +
+    `<div class="white-keys">${whiteKeys.join("")}</div>`;
+}
+
+function listenEvent(inputs, index) {
+  // index = -1 means listen all midi inputs
+  inputs.forEach((input, i) => {
+    if (i === index || index < 0) {
+      input.onmidimessage = onMessage;
+    } else {
+      input.onmidimessage = null;
+    }
+  });
+}
+
+function renderInputSelector(el, inputs) {
+  const id = "input-select";
+  const options = [];
+  options.push('<option value="-1">Listen all MIDI inputs</option>');
+  inputs.forEach((input, index) => {
+    options.push(
+      `<option value="${index}">${input.id} - ${input.manufacturer}</option>`
+    );
+  });
+  el.innerHTML = `<select id="${id}">${options.join("")}</select>`;
+  const select = document.getElementById(id);
+  select.addEventListener("change", (event) => {
+    const index = Number.parseInt(event.target.value);
+    listenEvent(inputs, index);
+  });
+}
+
+async function main() {
+  const piano = document.getElementById("piano");
+  const inputSelector = document.getElementById("input-selector");
+
+  try {
+    const access = await navigator.requestMIDIAccess();
+    const inputs = [...access.inputs.values()];
+    listenEvent(inputs, -1); // listen all midi input
+    renderPiano(piano);
+    renderInputSelector(inputSelector, inputs);
+  } catch (e) {
+    alert(e);
+  }
+}
+
+main();
